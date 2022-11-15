696446e8509b790480f081becd5887f45e342722
Pierrot LeCon <pstmrtem@outlook.com>
2022-11-15 20:29:56 +0100

    feat: file icon and bash lsp

--- nvim/lua/lsp_configuration.lua
+++ nvim/lua/lsp_configuration.lua
@@ -26,10 +26,12 @@ local null_ls = require("null-ls")
 local sources = {
 	null_ls.builtins.diagnostics.codespell,
 	null_ls.builtins.code_actions.proselint,
-	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.formatting.black,
 	null_ls.builtins.diagnostics.flake8,
 	null_ls.builtins.formatting.isort,
+	null_ls.builtins.formatting.stylua,
+	null_ls.builtins.code_actions.shellcheck,
+	null_ls.builtins.diagnostics.shellcheck,
 	null_ls.builtins.diagnostics.markdownlint,
 	null_ls.builtins.formatting.markdownlint,
 }
@@ -82,7 +84,7 @@ cmp.setup({
 				nvim_lsp = "λ",
 				luasnip = "⋗",
 				buffer = "Ω",
-				path = "🖫",
+				path = "",
 			}
 
 			item.menu = menu_icon[entry.source.name]
