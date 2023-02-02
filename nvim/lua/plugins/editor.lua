9d22a9b80608dbd2f7234a4957a8157b58568d4f
Pierre Pereira <pierre.pereira@jolibrain.com>
2023-02-02 17:37:49 +0100

    feat: enhanced folds

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -131,25 +131,4 @@ return {
 			"folke/lazy.nvim",
 		},
 	},
-	-- Show images directly in neovim.
-	{
-		"samodostal/image.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"m00qek/baleia.nvim", -- Color support.
-		},
-		opts = {
-			render = {
-				min_padding = 5,
-				show_label = true,
-				use_dither = true,
-				foreground_color = true,
-				background_color = true,
-			},
-			events = {
-				update_on_nvim_resize = true,
-			},
-		},
-		enabled = false,
-	},
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -124,4 +124,24 @@ return {
 			snippet_engine = "luasnip",
 		},
 	},
+	-- Enhanced folding.
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"kevinhwang91/promise-async",
+		},
+		config = function()
+			vim.opt.foldcolumn = '1'
+			vim.opt.foldlevel = 99
+			vim.opt.foldlevelstart = 99
+			vim.opt.foldenable = true
+
+			require("ufo").setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "treesitter", "indent" }
+				end
+			})
+		end,
+	},
 }
