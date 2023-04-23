7877efbaa2444b4f57c7c83d3c960f1cc0ac6228
Pierrot LeCon <pstmrtem@outlook.com>
2023-04-23 21:26:32 +0200

    fix(nvim): cmp select

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -72,15 +72,15 @@ return {
 					end,
 				},
 				mapping = {
-					["<Up>"] = cmp.mapping.select_prev_item({ cmp.SelectBehavior.Select, select = true }),
-					["<Down>"] = cmp.mapping.select_next_item({ cmp.SelectBehavior.Select, select = true }),
+					["<Up>"] = cmp.mapping.select_prev_item(),
+					["<Down>"] = cmp.mapping.select_next_item(),
 					["<C-d>"] = cmp.mapping.scroll_docs(4),
 					["<C-u>"] = cmp.mapping.scroll_docs(-4),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
-							cmp.select_next_item({ cmp.SelectBehavior.Select, select = true })
+							cmp.select_next_item()
 						elseif luasnip.expand_or_jumpable() then
 							luasnip.expand_or_jump()
 						else
@@ -89,7 +89,7 @@ return {
 					end, { "i", "s" }),
 					["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
-							cmp.select_prev_item({ cmp.SelectBehavior.Select, select = true })
+							cmp.select_prev_item()
 						elseif luasnip.jumpable(-1) then
 							luasnip.jump(-1)
 						else
