b6eb589b1ac854aaaa14228bea82cab4da275e72
Pierrot LeCon <pstmrtem@outlook.com>
2023-05-20 14:29:01 +0200

    feat(nvim): github theme, neorg filetype, simpler telescope fzf build

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -48,7 +48,7 @@ return {
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
 		lazy = true,
-		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		build = "make",
 	},
 	-- Telescope plugins installed by `lazy.nvim`.
 	{
