278805773883b94d2194f4862aa42a8d314caa76
Pierre Pereira <pierre.pereira@jolibrain.com>
2023-05-26 15:38:04 +0200

    fix: removed old fix

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -23,13 +23,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	end,
 })
 
--- Temporary fix, see https://github.com/neovim/neovim/issues/21856.
-vim.api.nvim_create_autocmd({ "VimLeave" }, {
-	callback = function()
-		vim.fn.jobstart('notify-send "hello"', { detach = true })
-	end,
-})
-
 -- Set filetypes.
 -- vim.filetype.add({
 -- 	extension = {
