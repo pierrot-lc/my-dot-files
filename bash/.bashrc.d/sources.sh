f4817563f50c786be7b9a182017f9c0e5825d91e
Pierrot LC <pstmrtem@outlook.com>
2023-06-09 21:24:11 +0200

    feat(bash): starship init

--- bash/.bashrc.d/sources.sh
+++ bash/.bashrc.d/sources.sh
@@ -4,3 +4,8 @@ export PATH="$HOME/.local/bin:$PATH"
 
 # Bob-nvim sources.
 export PATH="$HOME/.local/share/bob/nvim-bin:$PATH"
+
+if command -v starship > /dev/null 2>&1; then
+    eval "$(starship init bash)"
+    # cargo install starship
+fi
