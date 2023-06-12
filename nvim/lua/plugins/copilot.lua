36fc7073cd9502d9fe0bef696bd5d789728ae20e
Pierre Pereira <pierre.pereira@jolibrain.com>
2023-06-12 10:32:02 +0200

    feat(nvim): lowering the copilot priority in the cmp sources

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -31,14 +31,6 @@ return {
 		"zbirenbaum/copilot-cmp",
 		enabled = true,
 		dependencies = "zbirenbaum/copilot.lua",
-		config = function()
-			require("copilot_cmp").setup({
-				formatters = {
-					label = require("copilot_cmp.format").format_label_text,
-					insert_text = require("copilot_cmp.format").remove_existing,
-					preview = require("copilot_cmp.format").deindent,
-				},
-			})
-		end,
+		config = true,
 	},
 }
