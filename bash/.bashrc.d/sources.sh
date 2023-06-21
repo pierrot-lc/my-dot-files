ca0cd3a0a1159200b07b8179b63279a2fc418704
Pierrot LC <pstmrtem@outlook.com>
2023-06-21 21:14:16 +0200

    feat(bash): sourcing main.sh so that we can control the order of sourced files then

--- bash/.bashrc.d/sources.sh
+++ bash/.bashrc.d/sources.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
 
-# Bob-nvim sources.
-export PATH="$HOME/.local/share/bob/nvim-bin:$PATH"
+if command -v bob > /dev/null 2>&1; then
+    export PATH="$HOME/.local/share/bob/nvim-bin:$PATH"
+    # cargo install bob-nvim
+fi
 
 if command -v starship > /dev/null 2>&1; then
     eval "$(starship init bash)"
