69dd827d96198e517cb00aebc23b7b021e10ca34
Pierrot LC <pstmrtem@outlook.com>
2023-06-24 11:22:28 +0200

    feat(nvim): better mini pairs

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -28,7 +28,7 @@ return {
 	{
 		"echasnovski/mini.comment",
 		keys = {
-			{ "gc",  desc = "Comment line (visual mode)", mode = "v" },
+			{ "gc", desc = "Comment line (visual mode)", mode = "v" },
 			{ "gcc", desc = "Comment line" },
 			{ "dgc", desc = "Delete comment textobject" },
 			{ "vgc", desc = "Select comment textobject" },
@@ -105,7 +105,24 @@ return {
 	{
 		"echasnovski/mini.pairs",
 		event = "InsertEnter",
-		config = true,
+		opts = {
+			-- In which modes mappings from this `config` should be created
+			modes = { insert = true, command = false, terminal = false },
+
+			mappings = {
+				["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][^%)]" },
+				["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][^%]]" },
+				["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][^%}]" },
+
+				[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+				["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+				["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+
+				['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+				["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+				["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+			},
+		},
 	},
 	-- Easy splits and joins.
 	{
